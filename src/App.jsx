2c704e96607e2ec9531bf7354ac8281df14a4ec6
Shafry Yusuf <juni@Shafrys-MacBook-Air.local>
2026-04-23 20:05:03 +0700

    fix: hero pt-16 to avoid ticker overlap

--- src/App.jsx
+++ src/App.jsx
@@ -573,7 +573,7 @@ return (
           </div>
 
           {/* Transparency - Gallery Slider */}
-          <div className="relative">
+<div className="relative pt-16">
             <button 
               onClick={() => {
                 const container = galleryRef.current
